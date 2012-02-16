6e14832af308a72b08d6c59836eabbf53dcf6f1b
Daniel F. Moisset <dmoisset@machinalis.com>
2012-02-15 23:29:32 -0300

    Using int values to select category for now

--- django_search_views/forms.py
+++ django_search_views/forms.py
@@ -4,4 +4,4 @@ class SearchForm(forms.Form):
     q = forms.CharField(label='Search')
 
 class CategorySearchForm(SearchForm):
-    category = forms.ChoiceField(required=False)
+    category = forms.TypedChoiceField(required=False, coerce=int)
